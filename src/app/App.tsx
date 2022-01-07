435ce5cd7ffd295e6e4ed7a16c6946abbc0dd066
Demetriy <ArhDemetriy@GMail.com>
2022-01-07 22:40:46 +0800

    init Graph.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
 import { Controls } from './Controls/Controls';
+import { Graph } from './Graph/Graph';
 import { Table } from './Table/Table';
 
 export const App: React.FC = () => {
   return <div className="app">
-    <Table />
+    <div className="app-data">
+      <Table />
+      <Graph />
+    </div>
     <Controls />
   </div>
 }
---
+++ src/app/Graph/Graph.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Graph.scss';
+
+export const Graph: React.FC = () => {
+    return <div
+        className="graph"
+    >
+        Graph
+    </div>
+}
+
+export default Graph
+
