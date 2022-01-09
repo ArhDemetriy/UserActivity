c8542ae10ede3de1dc041f613526152243ce3858
Demetriy <ArhDemetriy@GMail.com>
2022-01-09 18:59:59 +0800

    off debug ver addUser button click

--- src/app/Controls/EventHandlers/AddUser.ts
+++ src/app/Controls/EventHandlers/AddUser.ts
@@ -6,14 +6,25 @@ let id = 0
 const DAY = 24 * 60 * 60 * 1000
 
 export const addUser: React.MouseEventHandler<HTMLButtonElement> = function (event) {
-    // UserActions.addUsers([
-    //     {
-    //         id: id,
-    //         registration: new Date(id++ * DAY),
-    //         lastActivity: new Date(),
-    //     },
-    // ])
-    UserActions.addUsers(getManyUsers())
+    const reduxUsers = getUsers()
+    if (!Array.isArray(reduxUsers) || !reduxUsers.length) {
+        UserActions.addUsers([getDefaultUser()])
+        return
+    }
+
+    const lastUser = reduxUsers[reduxUsers.length - 1]
+    UserActions.addUsers([{
+        ...getDefaultUser(),
+        id: lastUser.id + 1,
+    }])
+}
+
+function getDefaultUser(): Parameters<typeof UserActions['addUsers']>[0][0] {
+    return {
+        id: 1,
+        registration: new Date(),
+        lastActivity: new Date(),
+    }
 }
 
 function getManyUsers(count = 30) {
