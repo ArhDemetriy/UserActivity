dad37d75f909d69ad99ddc6ee07b5999a4e77c9d
Demetriy <ArhDemetriy@GMail.com>
2022-01-14 00:13:29 +0800

    dev getDateFromHTML in dateParsers.ts

--- src/app/Table/User/dateParser/dateParser.ts
+++ src/app/Table/User/dateParser/dateParser.ts
@@ -3,12 +3,14 @@ export function getHTMLDate(date: Date) {
     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
 }
 
+/** возвращает null прр ошибках */
 export function getDateFromHTML(date: string) {
-    if (!date?.split) { return new Date() }
-    const dateValues = date.trim().split('-').map(value => parseInt(value) || 0)
-    const newDate = new Date()
-    if (dateValues.length) {
-        newDate.setFullYear(dateValues[0], dateValues[1] - 1, dateValues[2])
-    }
+    if (!date?.split) { return null }
+    const dateValues = date.trim().split('-').map(value => parseInt(value))
+    if (dateValues.length < 3 || !dateValues.every(value => Number.isFinite(value) && value >= 0)) { return null }
+
+    const newDate = new Date(0)
+    newDate.setFullYear(dateValues[0], dateValues[1] - 1, dateValues[2])
+
     return newDate
 }
